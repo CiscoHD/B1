839165d8e6a580a38bccf94c6ae2e843b205e144
Francisco Uriel Pineda Hernández <323331535@alumno.enp.unam.mx>
2024-10-15 11:35:36 -0600

    Try 2

--- B.bat
+++ B.bat
@@ -1,15 +1,16 @@
 cd \
 
-md \Users\Public\Fotos\Photoshp
+md \Users\Public\Fotos\Photoshop
 md \Users\Public\Videos\Filmora
 
 move \B1\pracETEC.txt \Users\Public\Fotos\Photoshop
 move \B1\LibreETEC.txt \Users\Public\Videos\Filmora
+move \B1\Practica2Libre.docx 
 
-copy \B1\G61B \
-copy \B1\Grupo61D \
+move \B1\G61B \
+move \B1\Grupo61D \
 
-attrib +r \Users\Public\Fotos\Photosho\pracETEC.txt
+attrib +r \Users\Public\Fotos\Photoshop\pracETEC.txt
 attrib +r \Users\Public\Videos\Filmora\LibreETEC.txt
 
 path ;
